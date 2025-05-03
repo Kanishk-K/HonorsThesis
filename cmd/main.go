b082b786ec9f95e2534868cdf0a0751c2ae34c1c
Kanishk-K <kanishkkacholia@gmail.com>
2025-05-03 13:50:38 -0500

    👆 Allow command line arguments to configure runtime

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,93 @@ import (
 	"simulator/pkg/simulator"
 	"simulator/pkg/simulator/policies"
 	"simulator/pkg/workload"
+	"strconv"
 	"time"
 )
+
+func chooseRegion() string {
+	switch os.Args[1] {
+	case "CAISO":
+		return "CAISO.csv"
+	case "ERCOT":
+		return "ERCOT.csv"
+	case "MISO":
+		return "MISO.csv"
+	case "NYISO":
+		return "NYISO.csv"
+	default:
+		panic("Invalid region specified. Please choose CAISO, ERCOT, MISO, or NYISO.")
+	}
+}
+
+func chooseSLO() time.Duration {
+	switch os.Args[2] {
+	case "1hr":
+		return 1 * time.Hour
+	case "6hr":
+		return 6 * time.Hour
+	case "12hr":
+		return 12 * time.Hour
+	case "24hr":
+		return 24 * time.Hour
+	default:
+		panic("Invalid SLO specified. Please choose 1hr, 6hr, 12hr, or 24hr.")
+	}
+}
+
+func chooseWorkload() string {
+	switch os.Args[3] {
+	case "random":
+		return "random"
+	case "uniform":
+		return "uniform"
+	case "morningSpike":
+		return "morningSpike"
+	case "afternoonSpike":
+		return "afternoonSpike"
+	case "eveningSpike":
+		return "eveningSpike"
+	case "nightSpike":
+		return "nightSpike"
+	case "weekdaySpike":
+		return "weekdaySpike"
+	case "weekendSpike":
+		return "weekendSpike"
+	default:
+		panic("Invalid workload specified. Please choose random, uniform, or normal.")
+	}
+}
+
+func choosePolicy() simulator.PolicyInterface {
+	switch os.Args[4] {
+	case "fifo":
+		model, err := directory.FetchDirectory().GetModelDefinition(os.Args[5])
+		if err != nil {
+			panic("Error getting model definition")
+		}
+		return policies.NewFIFO(model)
+	case "temporal":
+		model, err := directory.FetchDirectory().GetModelDefinition(os.Args[5])
+		if err != nil {
+			panic("Error getting model definition")
+		}
+		return policies.NewTemporal(model, 0)
+	case "modelSelection":
+		accuracy, err := strconv.ParseFloat(os.Args[5], 64)
+		if err != nil {
+			panic("Error parsing accuracy")
+		}
+		return policies.NewModelSelection(accuracy)
+	case "hybridSelection":
+		accuracy, err := strconv.ParseFloat(os.Args[5], 64)
+		if err != nil {
+			panic("Error parsing accuracy")
+		}
+		return policies.NewHybridSelection(accuracy, 0)
+	default:
+		panic("Invalid policy specified. Please choose fifo, temporal, modelSelection, or hybridSelection.")
+	}
+}
 
 func main() {
 	currDir, err := os.Getwd()
@@ -22,7 +107,7 @@ func main() {
 	/*
 		Load in carbon emission data
 	*/
-	dataPath := filepath.Join(currDir, "..", "data", "collected", "ERCOT.csv")
+	dataPath := filepath.Join(currDir, "..", "data", "collected", chooseRegion())
 	dataLoader := loader.NewLoader(dataPath)
 	log.Println(dataLoader)
 
@@ -30,24 +115,18 @@ func main() {
 		Load in AI Model Definitions & Workload information
 	*/
 	modelPath := filepath.Join(currDir, "..", "cmd", "AIModels.json")
-	modelDirectory := directory.NewDirectory(modelPath)
+	_ = directory.NewDirectory(modelPath)
 
 	/*
 		Generate and load in workload information
 	*/
-	jobInfo := workload.NewJobInfo(6*time.Hour, 1000000, "random")
+	jobInfo := workload.NewJobInfo(chooseSLO(), 1000000, chooseWorkload())
 	workload := workload.GetWorkload(jobInfo)
 
 	/*
 		Initialize the simulator
 	*/
-	model, err := modelDirectory.GetModelDefinition("small")
-	if err != nil {
-		log.Println("Error getting model definition:", err)
-		return
-	}
-	schedulingPolicy := policies.NewTemporal(model, 0)
-	simElement := simulator.NewSimulator(workload.Jobs, schedulingPolicy)
+	simElement := simulator.NewSimulator(workload.Jobs, choosePolicy())
 	if simElement == nil {
 		log.Println("Simulator not initialized. Exiting.")
 		return
--- test.sh
+++ test.sh
@@ -1,7 +1,22 @@
 cd cmd
+
+for i in $(seq 42 100)
+do
+  go run main.go CAISO 6hr random hybridSelection 0.7 >> ../summary_CAISO_6hr_random_hybridSelection_70.log
+  echo "Iteration $i/100"
+  find . -name "*.log" -type f -delete
+done
+
 for i in $(seq 1 100)
 do
-  go run main.go >> ../summary_ERCOT_6hr_random_temporal_small.log
+  go run main.go CAISO 6hr random hybridSelection 0.8 >> ../summary_CAISO_6hr_random_hybridSelection_80.log
+  echo "Iteration $i/100"
+  find . -name "*.log" -type f -delete
+done
+
+for i in $(seq 1 100)
+do
+  go run main.go CAISO 6hr random hybridSelection 0.9 >> ../summary_CAISO_6hr_random_hybridSelection_90.log
   echo "Iteration $i/100"
   find . -name "*.log" -type f -delete
 done
